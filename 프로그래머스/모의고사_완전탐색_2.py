e01ebf9a3f60a6dcf6677fd275c047c9cc231214
mumu <tnddls2ek@naver.com>
2021-04-01 14:45:10 +0900

    스터디 모의고사2 다시풀기

--- 프로그래머스/모의고사_완전탐색_2.py
+++ 프로그래머스/모의고사_완전탐색_2.py
@@ -12,7 +12,6 @@ def solution(answers):
     one_arr = [1,2,3,4,5]
     two_arr = [2,1,2,3,2,4,2,5]
     three_arr = [3,3,1,1,2,2,4,4,5,5]
-
     answer_count_arr = [0,0,0]
 
     for (one,two,three,item) in zip(cycle(one_arr),cycle(two_arr),cycle(three_arr),answers):
@@ -27,7 +26,6 @@ def solution(answers):
         if item == max(answer_count_arr):
             answer.append(index+1)
 
-    print(answer)
     return answer
 
 solution([1,2,3,4,5])
