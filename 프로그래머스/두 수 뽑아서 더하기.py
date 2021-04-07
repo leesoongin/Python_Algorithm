c58b2309e845d58a04c9f9bde442672b0fba893f
mumu <tnddls2ek@naver.com>
2021-04-07 23:18:03 +0900

    두 수 뽑아서 더하기 - 큐

---
+++ 프로그래머스/두 수 뽑아서 더하기.py
@@ -0,0 +1,21 @@
+# 정수 배열 numbers가 주어집니다.
+# numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+
+
+# 입출력 예
+# numbers	result
+# [2,1,3,4,1] -- > 	[2,3,4,5,6,7]
+# [5,0,2,7]	-- > [2,5,7,9,12]
+# 정렬 ,,ㅅㅂ,,,
+
+def solution(numbers):
+    answer = set()
+
+    while numbers:
+        target = numbers.pop(0)
+        for item in numbers:
+            answer.add(item + target)
+    return sorted(list(answer))
+
+
+solution([2,1,3,4,1])
