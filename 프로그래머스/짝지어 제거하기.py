55474a0e253a7df9e81f65efdaec5a32f17f8e40
mumu <tnddls2ek@naver.com>
2021-05-07 21:10:11 +0900

    짝지어 제거하기 - stack

---
+++ 프로그래머스/짝지어 제거하기.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(s):
+    stack = deque()
+
+    for item in s:
+        if len(stack) == 0:
+            stack.append(item)
+        elif stack[-1] == item:
+            stack.pop()
+        else:
+            stack.append(item)
+
+    # 다 된경우
+    if len(stack) == 0:
+        return 1
+    return 0
+
+
+print(solution("baabaa"))
