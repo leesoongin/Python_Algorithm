805f3e080470721c8662c1cb08f9e135032636ad
mumu <tnddls2ek@naver.com>
2021-05-13 16:27:20 +0900

    프로그래머스 -3진법 뒤집기

---
+++ 프로그래머스/하루 한문제/3진법 뒤집기.py
@@ -0,0 +1,28 @@
+def solution(n):
+    answer = 0
+    sam = list()
+
+    while n > 0:
+        n,mod = divmod(n,3)
+        sam.append(mod)
+    for i in range(len(sam)):
+        answer += sam[i] * 3**(len(sam)-(1+i))
+
+    return answer
+
+def solution2(n):
+    sam = list()
+
+    while n > 0:
+        n,mod = divmod(n,3)
+        sam.append(str(mod))
+    answer = int(''.join(sam),3)
+
+    return answer
+
+print(solution(45))
+print(solution2(45))
+
+
+# n (10진법)	n (3진법)	앞뒤 반전(3진법)	10진법으로 표현
+# 45	1200	0021	7
