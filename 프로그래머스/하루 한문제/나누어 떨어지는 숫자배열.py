791a0d2b4e52f63e27f676583544efc578b72381
mumu <tnddls2ek@naver.com>
2021-05-13 17:01:26 +0900

    프로그래머스 - 나누어 떨어지는 숫자배열

---
+++ 프로그래머스/하루 한문제/나누어 떨어지는 숫자배열.py
@@ -0,0 +1,16 @@
+def solution(arr, divisor):
+    answer = []
+
+    for item in arr:
+        if item % divisor == 0:
+            answer.append(item)
+    if len(answer) == 0:
+        answer.append(-1)
+
+    return sorted(answer)
+
+
+# 지린다 ㄹㅇ,,
+# or은 앞에꺼가 참이라면 실행되지않고 거짓이라면 뒤까지 실행됨
+def solution(arr, divisor):
+    return sorted([item for item in arr if item % divisor == 0]) or [-1]
