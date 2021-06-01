ec51cd5b4b8e73b098357f20d95e7f2f265a3b8c
mumu <tnddls2ek@naver.com>
2021-06-01 17:01:49 +0900

    프로그래머스 - 타겟넘버

---
+++ 프로그래머스/타겟넘버.py
@@ -0,0 +1,11 @@
+from itertools import product
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)
+
+
+
+print(solution([1,1,1,1,1],3))
+# numbers	target	return
+# [1, 1, 1, 1, 1]	3	5
